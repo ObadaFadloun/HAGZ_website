d0e5bbdaaa1aa715f7e32b4fb1f4490a8453ee67
ObadaFadloun <obada.fadloun@gmail.com>
2025-09-02 05:25:07 +0300

    implement authentication controller with register and login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const signToken = (id) => {
+  return (token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  }));
+};
+
 exports.register = catchAsync(async (req, res) => {
   try {
     const { firstName, lastName, email, password, confirmPassword } = req.body;
 
-    // Check all required fields
     if (!(firstName && lastName && email && password && confirmPassword)) {
       return res.status(400).json({
         status: 'fail',
@@ -23,15 +29,12 @@ exports.register = catchAsync(async (req, res) => {
       confirmPassword
     });
 
-    // generate a jwt token.
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN
-    });
+    const token = signToken(user._id);
 
     res.status(201).json({
       status: 'success',
-      token,
-      data: user
+      data: user,
+      token
     });
   } catch (err) {
     console.error(err);
@@ -57,17 +60,58 @@ exports.login = catchAsync(async (req, res, next) => {
       return next(new AppError('Your email or password is not correct', 401));
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN
-    });
+    const token = signToken(user._id);
 
     res.status(200).json({
       status: 'success',
-      token,
-      data: user
+      data: user,
+      token
     });
   } catch (err) {
     console.error(err);
     return next(new AppError('Server error', 500));
   }
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not looged in! Please log in to get access.', 401)
+    );
+  }
+
+  // 2) verification token.
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) check if user still exists
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
+
+  // 4) check if user changed password after the tiken was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! please log in again.', 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
 });
