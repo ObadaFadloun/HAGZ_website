f6a4389f97c41ed65eb2674ee6bd7243ab4c48c9
ObadaFadloun <obada.fadloun@gmail.com>
2025-09-07 14:56:05 +0300

    refactor user routes to correctly import UserController and organize route definitions

---
+++ controllers/userController.js
@@ -0,0 +1,66 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  if (!users) {
+    return next(new AppError('No users found!', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
+  });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  // User.findOne({ _id: req.params.id })
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!user) {
+    return next(new AppError('No user fount with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(new AppError('No user fount with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User was deleted successfully'
+  });
+});
--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,10 +1,18 @@
-const UserController = require('../controllers/authController');
+const AuthController = require('../controllers/authController');
+const UserController = require('../controllers/userController');
 
 const express = require('express');
 
 const router = express.Router();
 
-router.route('/register').post(UserController.register);
-router.route('/login').post(UserController.login);
+router.route('/').get(UserController.getAllUsers);
+router.route('/register').post(AuthController.register);
+router.route('/login').post(AuthController.login);
+
+router
+  .route('/:id')
+  .get(UserController.getUser)
+  .patch(UserController.updateMe)
+  .delete(UserController.deleteMe);
 
 module.exports = router;
