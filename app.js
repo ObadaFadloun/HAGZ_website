a79cb04278e56188cd27fa1efedbd1a9e992eeb3
ObadaFadloun <obada.fadloun@gmail.com>
2025-09-19 08:13:05 +0300

    refactor app.js; reorganize middleware setup for improved security and request handling

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 const dotenv = require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const morgan = require('morgan');
-const userRouter = require('./routes/userRouter');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSnaitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globelErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRouters');
 
 const app = express();
 
-//middlewares
+// 1) Globel middleware
+// Set security HTTP headers
+app.use(helmet());
 
-app.use(express.json());
-app.use(morgan('dev'));
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-//test url
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Hello there! 😊'
-  });
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSnaitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    // whitelist: ['duration']
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/api/v1/users', userRouter);
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use('/api/v1/user', userRouter);
-
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
-});
+app.use(globelErrorHandler);
 
 module.exports = app;
