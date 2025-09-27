ec36c7fbadf3e13c60efea62dfc44f5b80502106
ObadaFadloun <obada.fadloun@gmail.com>
2025-09-27 08:04:37 +0300

    feat: Initialize project with essential files and dependencies
    - Added package.json with project metadata and dependencies.
    - Created userRouters.js for user authentication and management routes.
    - Implemented server.js to set up the Express server and connect to MongoDB.
    - Added utility functions for error handling (appError.js) and async error catching (catchAsync.js).
    - Developed email utility (email.js) for sending emails using nodemailer.
    - Created footballFieldRouters.js for future use in managing football fields.

---
+++ New folder/routes/userRouters.js
@@ -0,0 +1,56 @@
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+// router.post('/logout', userController.logout);
+
+// User Profile & Actions (Protected - requires authentication)
+// router.get('/profile', authUser, userController.getUserProfile);
+// router.put('/profile', authUser, userController.updateUserProfile);
+
+// User's Booking History (Protected)
+// router.get('/my-bookings', authUser, userController.getMyBookings);
+
+// User's Reviews (Protected)
+// router.get('/my-reviews', authUser, userController.getMyReviews);
+
+// Team Management for the User (Protected)
+// router.get('/my-teams', authUser, userController.getMyTeams);
+// router.post('/join-team', authUser, userController.joinTeam); // Using an invite code/link
+
+// Loyalty Program (Protected)
+// router.get('/loyalty-points', authUser, userController.getLoyaltyPoints);
+
+// Inbox/Messages (Protected)
+// router.get('/inbox', authUser, userController.getMyMessages);
+// router.post('/inbox/send', authUser, userController.sendMessage);
+// router.get('/inbox/:conversationId', authUser, userController.getConversation);
+
+// Route to request becoming an Owner (Protected)
+// router.post('/request-owner', authUser, userController.requestOwnerStatus);
+
+router.route('/').get(authController.protect, userController.getAllUsers);
+// router
+//   .route('/:id')
+//   .get(userController.getUser)
+//   .patch(userController.updateMe)
+//   .delete(userController.deleteMe);
+
+module.exports = router;
