065992cae06ef5204a53e5995f889c92c6ed80a9
ObadaFadloun <obada.fadloun@gmail.com>
2025-10-09 21:16:51 +0300

    feat: implement user profile image upload and resizing functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
+const sharp = require('sharp'); // 🟢 You need this for image resizing
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const fs = require('fs');
+const path = require('path');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -9,45 +12,77 @@ const filterObj = (obj, ...allowedFields) => {
       newObj[el] = obj[el];
     }
   });
+  return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
+// 🧩 1. Middleware to handle single image upload (field: profileImage)
+exports.uploadUserPhoto = require('../middleware/upload').single(
+  'profileImage'
+);
 
-  if (!users) {
-    return next(new AppError('No users found!', 404));
+// 🧩 2. Resize image and set its path in req.body
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) {
+    return next();
   }
+
+  const uploadDir = path.join(__dirname, '../public/img/users');
+
+  // 🔧 Ensure folder exists
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  const filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(path.join(uploadDir, filename));
+
+  req.body.profileImage = `${req.protocol}://${req.get('host')}/img/users/${filename}`;
+  next();
+});
+
+// 🧩 3. Update profile picture field in DB
+exports.updateProfilePicture = catchAsync(async (req, res, next) => {
+  if (!req.body.profileImage) {
+    return next(new AppError('Please upload an image first', 400));
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { profileImage: req.body.profileImage },
+    { new: true, runValidators: true }
+  );
 
   res.status(200).json({
     status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
+    user: updatedUser
   });
 });
 
+// 🧩 4. Update profile info (no passwords or image here)
 exports.updateMe = catchAsync(async (req, res, next) => {
-  // 1) Create error if user POSTs password data
   if (req.body.password || req.body.confirmPassword) {
     return next(
       new AppError(
-        'This route is not for password updates. please use /updateMyPassword',
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  } else if (req.body.profileImage) {
+    return next(
+      new AppError(
+        'This route is not for profile picture updates. Please use /updateProfilePicture',
         400
       )
     );
   }
 
-  // 2) Filtered out unwanted fields that are not allowed to be updated
-  const filteredBody = filterObj(
-    req.body,
-    'firstName',
-    'lastName',
-    'email',
-    'profileImage'
-  );
+  const filteredBody = filterObj(req.body, 'firstName', 'lastName', 'email');
 
-  // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
@@ -55,9 +90,45 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
-    data: {
-      user: updatedUser
-    }
+    user: updatedUser
+  });
+});
+
+// 🧩 5. Request user to be an Owner
+exports.requestOwner = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    { ownerRequestStatus: 'pending' },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Owner request sent successfully!',
+    user
+  });
+});
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  if (!users) {
+    return next(new AppError('No users found!', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: { users }
+  });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { user }
   });
 });
 
@@ -67,21 +138,5 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   res.status(204).json({
     status: 'success',
     data: null
-  });
-});
-
-exports.getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  // User.findOne({ _id: req.params.id })
-
-  if (!user) {
-    return next(new AppError('No user found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
   });
 });
